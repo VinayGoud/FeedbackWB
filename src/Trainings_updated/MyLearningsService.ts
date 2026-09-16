9dca5422437ac505a2887e68c237f182dd99d084
VinayGoud <30427760+VinayGoud@users.noreply.github.com>
2026-09-16 18:52:33 -0400

    Implement caching for training details in MyLearningsService
    Refactor MyLearningsService to include caching for training details.

--- src/Trainings_updated/MyLearningsService.ts
+++ src/Trainings_updated/MyLearningsService.ts
@@ -1,9 +1,7 @@
 import { WebPartContext } from '@microsoft/sp-webpart-base';
-import { spfi, SPFx } from '@pnp/sp';
-
-import '@pnp/sp/profiles';
 
 import { APIService } from '../../../adaptiveCardExtensions/training/Services/APIService';
+import { Constants } from '../../../adaptiveCardExtensions/training/Utilities/Constants';
 import { ITrainings } from '../../../adaptiveCardExtensions/training/Utilities/Interfaces';
 
 export class MyLearningsService {
@@ -13,20 +11,52 @@ export class MyLearningsService {
     this._context = context;
   }
 
-  public async getUserId(): Promise<[string, boolean]> {
-    const sp = spfi().using(SPFx(this._context));
-
-    return APIService.getUserId(sp, this._context);
-  }
-
   public async getTrainingDetails(
     userId: string,
     useStageConnection: boolean
   ): Promise<ITrainings> {
+
+    const cachedTrainingDetails = this._getCachedTrainingDetails();
+
+    if (cachedTrainingDetails) {
+      return cachedTrainingDetails;
+    }
+
     return APIService.getTrainingDetails(
       this._context,
       userId,
       useStageConnection
     );
+  }
+
+  private _getCachedTrainingDetails(): ITrainings | undefined {
+    try {
+      const trainingDetails = localStorage.getItem(
+        Constants.localStorageTrainingKey
+      );
+
+      const expiryTime = localStorage.getItem(
+        Constants.localStorageExpiryKey
+      );
+
+      if (!trainingDetails || !expiryTime) {
+        return undefined;
+      }
+
+      const expiryDate = new Date(expiryTime);
+
+      if (new Date() >= expiryDate) {
+        return undefined;
+      }
+
+      return JSON.parse(trainingDetails) as ITrainings;
+    } catch (error) {
+      console.error(
+        '[MyLearningsService] Failed to read cached training details.',
+        error
+      );
+
+      return undefined;
+    }
   }
 }

aef94e10dd80c72b580c2f8a4d593d7202aa10e7
VinayGoud <30427760+VinayGoud@users.noreply.github.com>
2026-09-16 12:23:46 -0400

    Add en-us.js for localization of learning content

---
+++ src/MyLearnings/en-us.js
@@ -0,0 +1,16 @@
+define([], function() {
+  return {
+    "Title": "My Learning",
+    "NoTrainingTitle": "Training",
+    "MandatoryTrainingDescription": "Explore 'My Dashboard' in 3M Learn for all assigned learning, training, and certifications.",
+    "NoMandatoryTrainingPrimaryText": "Explore 3M Learn to find assigned training and personalized learning for career development.",
+    "SingleMandatoryTrainingPrimaryText": "mandatory course assigned",
+    "MultipleMandatoryTrainingPrimaryText": "mandatory courses assigned",
+    "ViewCourseLabel": "View Course",
+    "VisitButtonLabel": "Explore 3M Learn",
+    "CardButtonTarget": "https://3m.csod.com",
+    "ErrorMessage": "Oops! Something went wrong. Please refresh your screen.",
+    "ContactUsButtonText": "Contact us",
+    "ContactUsLink": "#"
+  }
+});
